84757e3e596578dfd1c0dc69a81b8d49b7a8c932
Rômulo Júnio <86726833+Romz16@users.noreply.github.com>
2022-02-14 17:14:17 -0300

    Feat: Adiciona sistema de nivel e pontuação

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,10 +5,28 @@ print("*********************************")
 print("Bem vindo ao jogo de Advinhação!")
 print("*********************************")
 
-numero_secreto = (random.randrange(1,101))
 
-tentativas = 3
+numero_secreto = (random.randrange(1,101))
+tentativas = 0
 rodada = 1 
+pontos = 1000
+
+print("Qual dificuldade deseja? \n")
+print("1-fácil\n2-Médio\n3-Difícil\n")
+nivel = int( input("Digite o nivel"))
+
+while(nivel>3 or nivel<1):
+    print("Erro, selecione uma dificuldade valida\n")
+    print("Qual dificuldade deseja? \n")
+    print("1-fácil\n2-Médio\n3-Difícil\n")
+    nivel = int( input("Digite o nivel"))
+
+if nivel ==1:
+        tentativas = 20
+elif nivel ==2:
+        tentativas =10
+elif nivel ==3:
+        tentativas =5
 
 for rodada in range (1,tentativas+1):
     print("Tentativa: {} de {}".format(rodada,tentativas))
@@ -24,14 +42,16 @@ for rodada in range (1,tentativas+1):
     menor = numero_secreto > chute
 
     if (acertou):
-        print("você acertou!")
+        print("você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("Você errou!! O seu chute foi maior que o numero secreto.")
         elif(menor):
             print("Você errou!! O seu chute foi menor que o numero secreto.") 
-            
-if rodada == 3:
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+        
+if rodada == tentativas:
     print("O numero secreto era:",numero_secreto)
 print("Fim de jogo")
