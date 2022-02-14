c4cfc2a4a9c00cc1cd3eb25dbef4d486a972b710
Rômulo Júnio <86726833+Romz16@users.noreply.github.com>
2022-02-14 15:39:52 -0300

    Feat: Adição de loop de tentativas no jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,9 +3,27 @@ print("Bem vindo ao jogo de Advinhação!")
 print("*********************************")
 
 numero_secreto = 42
-chute =  int( input("Digite o seu número: "))
-print("você digitou o número : ", chute)
-if (numero_secreto == chute):
-    print("você acertou")
-else:
-    print("voce errou")
+
+tentativas = 3
+rodada = 1 
+
+
+while(rodada<=tentativas):
+    print("Tentativa: ", rodada,"de", tentativas)
+    chute =  int( input("Digite o seu número: "))
+    print("você digitou o número : ", chute)
+
+    acertou = numero_secreto == chute
+    maior = numero_secreto <chute
+    menor = numero_secreto > chute
+
+    if (acertou):
+        print("você acertou!")
+        rodada = rodada +10
+    else:
+        if(maior):
+            print("Você errou!! O seu chute foi maior que o numero secreto.")
+        elif(menor):
+            print("Você errou!! O seu chute foi menor que o numero secreto.") 
+        rodada = rodada + 1 
+print("Fim de jogo")
