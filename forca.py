7e8a5756a82d3182f5496899e3238febaeca8d42
Rômulo Júnio <86726833+Romz16@users.noreply.github.com>
2022-02-15 14:21:11 -0300

    Fix: Soluciona problema de jogo sem fim
    Agora o jogo termina quando se erra 6 vezes ou quando se acerta todas as letras

--- forca.py
+++ forca.py
@@ -3,21 +3,28 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    palavra_secretra = "banana"
+    palavra_secretra = "banana".upper()
     letras_acertadas =["_","_","_","_","_","_"]
     enforcou =False
     acertou = False
+    erros = 0
     
     print(letras_acertadas)
     print("\n ")
     while(not enforcou and not acertou):
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         index = 0
-        for letra in palavra_secretra:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index]=letra
-            index = index +1
+        if chute in palavra_secretra:
+            for letra in palavra_secretra:
+                if(chute== letra):
+                    letras_acertadas[index]=letra
+                index = index +1
+        else:
+            erros = erros +1
+            
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
                 
         print(letras_acertadas)
         print("\n ")
