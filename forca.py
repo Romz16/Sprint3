b96b7a704fe18825406151a99b662b50b0dbf695
Rômulo Júnio <86726833+Romz16@users.noreply.github.com>
2022-02-15 14:05:29 -0300

    Feat: Adicionado capacidade de jogar
    O programa agora tem as funcionalidades para jogar forca, porem não está completamente  funcional

--- forca.py
+++ forca.py
@@ -3,5 +3,26 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
+    palavra_secretra = "banana"
+    letras_acertadas =["_","_","_","_","_","_"]
+    enforcou =False
+    acertou = False
+    
+    print(letras_acertadas)
+    print("\n ")
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secretra:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index]=letra
+            index = index +1
+                
+        print(letras_acertadas)
+        print("\n ")
 
     print("Fim de jogo")
+    
+if (__name__=="__main__"):
+    jogar()
